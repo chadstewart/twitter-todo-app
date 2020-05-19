75706a671d595aea7c358c7115ae95ac7de4f91e
Chad Stewart <chadrhonanstewart@gmail.com>
2020-05-19 07:33:49 -0500

    Made some progress. Redirects to home, can pass data through add route, have the skeleton for delete ready.

--- server.js
+++ server.js
@@ -1,18 +1,36 @@
 // Setup server
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
 // ------------------ Don't change above this line ------------------
 
-
+//Request Data Type
+app.use(express.urlencoded());
 
 // First route. Go to http://localhost:5000 and you should see Hello World in the
 // browser. Remember the project has no UI, so this is just for you to experience
 // that this all works.
-app.get('/', (req, res) => res.send('Hello World!!'))
+
+//Initial Landing
+app.get('/', (req, res) => res.redirect('/home'));
+app.get('/home', (req, res) => res.send('Hiho!!'));
+
+//New Entry
+app.post('/add', function (req, res) {
+
+    res.send(`You added ${req.body.entry}!`);
+
+});
+
+//Delete Entry
+app.delete('/remove', function(req, res) {
+
+    res.send('Delete Successful!');
+
+});
 
 
 
 // -------------------- Don't change below this line ------------------
 // Setup where the server listens e.g. which port. Necessary for the browser for example.
-const PORT = process.env.PORT || 5000
-app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`))
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
